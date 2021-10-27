47f0bd46de1bc85c4be073da9e0af7725e0820b6
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2021-10-26 23:44:07 -0500

    camera working and free movement of player

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _player, _playerCamera, _focusPoint;
+    [SerializeField]
+    private float _playerHeight=2f;
+    #region VariablesZoom
+    [SerializeField]
+    private float _zoom=-7, _zoomSpeed = 4, _zoomMin = -7, _zoomMax = 0;
+    [SerializeField]
+    private float _camRotX, _camRotY;
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        #region Asignacion de objeto
+        if(_player==null)
+        {
+            Debug.LogWarning("Se debe asignar el jugador desde el inspector");
+        }
+        if(_playerCamera==null)
+        {
+            Debug.LogWarning("Se debe asignar la camara desde el inspector");
+        }
+        if(_player==null)
+        {
+            Debug.LogWarning("Se debe asignar el punto de foco desde el inspector");
+        }
+        #endregion
+
+        #region Parentezco
+        // _focusPoint.transform.SetParent(_player);
+        _playerCamera.transform.SetParent(_focusPoint);
+        #endregion
+
+        #region Inicializa Zoom
+        if(_zoom > _zoomMax)
+        {
+            _zoom = -7;
+        }
+        _playerCamera.transform.localPosition = new Vector3(0,0,_zoom);
+        _focusPoint.transform.localPosition = new Vector3(0,_playerHeight,0);
+        _playerCamera.LookAt(_focusPoint);
+        #endregion
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        #region focusPointCopyPositionPlayer
+        _focusPoint.transform.localPosition = new Vector3(_player.transform.localPosition.x, _player.transform.localPosition.y+_playerHeight, _player.transform.localPosition.z);
+        #endregion
+
+
+        #region Zoom
+        _zoom += Input.GetAxis("Mouse ScrollWheel") * _zoomSpeed;
+        _zoom = Mathf.Clamp(_zoom,_zoomMin,_zoomMax);
+        _playerCamera.transform.localPosition = new Vector3(0,0, _zoom);
+        #endregion
+
+        #region rotacion camara
+        if(Input.GetMouseButton(1))
+        {
+            _camRotX += Input.GetAxis("Mouse X");
+            _camRotY += Input.GetAxis("Mouse Y");
+            _focusPoint.transform.localRotation = Quaternion.Euler(-_camRotY, _camRotX, 0);
+        }
+        #endregion
+    }
+}
--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class CharacterScript : MonoBehaviour
 {
-    public float velocidadMovimiento = 5.0f;
-    public float velocidadRotacion = 200.0f;
+    [SerializeField]
+    private Transform _playerFocusPoint, _player;
+    [SerializeField]
+    private float velocidadMovimiento = 5.0f;
+    [SerializeField]
+    private float velocidadRotacion = 200.0f;
     private Animator anim;
-    public float x,y;
+    [SerializeField]
+    private float x,y;
+
+    private float cameraY, playerY;
 
     // Start is called before the first frame update
     void Start()
     {
         anim = GetComponent<Animator>();
+
+        if(_playerFocusPoint==null)
+        {
+            Debug.LogWarning("Se debe asignar la camara del jugador desde el inspector");
+        }
     }
 
     // Update is called once per frame
@@ -21,8 +33,38 @@ public class CharacterScript : MonoBehaviour
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
 
-        transform.Rotate(0,x*Time.deltaTime*velocidadRotacion,0);
+        cameraY = _playerFocusPoint.eulerAngles.y;
+        // if(cameraY > 180f)
+        // {
+        //     cameraY = cameraY - 360f;
+        // }
+        playerY = _player.eulerAngles.y;
+        // if(playerY > 180f)
+        // {
+        //     playerY = playerY - 360f;
+        // }
+            Debug.Log(playerY);
+            Debug.Log(cameraY);
+
+        if(y != 0 && x == 0)
+        {
+            // (cameraY - playerY < (360-cameraY)+playerY) && !Mathf.Approximately(cameraY , playerY)
+            // (cameraY < playerY && (cameraY - playerY) < 180) || (cameraY > playerY && ((cameraY+360) - playerY) < 180)
+            // if(cameraY > playerY )
+            if(((cameraY - playerY) > 0 && (cameraY - playerY) < 180) || ((cameraY+360) - playerY < 180) )
+            {
+                x = 1;
+            }
+            else
+            {
+                x = -1;
+            }  
+        }
+        
+        
+
         transform.Translate(0,0,y*Time.deltaTime*velocidadMovimiento);
+        transform.Rotate(0,x*Time.deltaTime*velocidadRotacion,0);
 
         anim.SetFloat("VelX",x);
         anim.SetFloat("VelY",y);
